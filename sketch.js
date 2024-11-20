f4ca59d0e3d5b69d10cea671e3b8c0684231d179
ALLISON PRYOR <ARP21C@FSU.EDU>
2024-11-20 10:37:49 -0500

    all sprites made and moving

--- sketch.js
+++ sketch.js
@@ -3,6 +3,15 @@ let belt;
 let beltSpeed = 2;
 let beltDirectionX = 1;
 let beltDirectionY = 1;
+let car;
+let carSpeed = 2;
+let carDirectionX = 1;
+let carDirectionY = 1;
+let house;
+let houseSpeed = 2;
+let houseDirectionX = 1;
+let houseDirectionY = 1;
+
 
 function setup() {
   createCanvas(800, 600);
@@ -14,6 +23,14 @@ function setup() {
   // Create the "belt" sprite
   belt = new Sprite(width * 0.1, height * 0.9, 30, 30); // Positioned at the bottom-left corner
   belt.color = 'green';
+
+  //create the "car" sprite
+  car = new Sprite(width * 0.5, height * 0.3, 30, 30);
+
+  // Create the "house" sprite
+  house = new Sprite(width * 0.2, height * 0.5, 30, 30); // Positioned at the bottom-left corner
+  
+  
 }
 
 function draw() {
@@ -36,6 +53,13 @@ function draw() {
   belt.x += beltSpeed * beltDirectionX;
   belt.y += beltSpeed * beltDirectionY;
 
+  //move the "car" sprite
+  car.x += carSpeed * carDirectionX;
+  car.y += carSpeed * carDirectionY;
+
+  house.x += houseSpeed * houseDirectionX;
+  house.y += houseSpeed * houseDirectionY;
+
   // Reverse direction on hitting canvas boundaries
   if (belt.x < belt.w / 2 || belt.x > width - belt.w / 2) {
     beltDirectionX *= -1; // Reverse horizontal movement
@@ -44,9 +68,21 @@ function draw() {
     beltDirectionY *= -1; // Reverse vertical movement
   }
 
-  // Check for collisions
-  if (player.collides(belt)) {
-    belt.x = random(50, width - 50);
-    belt.y = random(50, height - 50);
+  //reverse direction on hitting canvas boundaries
+  if (car.x < car.w /2 || car.x > width - car.w /2) {
+	carDirectionX *= -1;
   }
+  if (car.y < car.h / 2 || car.y > height - car.h / 2) {
+    carDirectionY *= -1; // Reverse vertical movement
+  }
+
+  // Reverse direction on hitting canvas boundaries
+  if (house.x < house.w / 2 || house.x > width - house.w / 2) {
+    houseDirectionX *= -1; // Reverse horizontal movement
+  }
+  if (house.y < house.h / 2 || house.y > height - house.h / 2) {
+    houseDirectionY *= -1; // Reverse vertical movement
+  }
+
+ 
 }
